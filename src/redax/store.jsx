79013b5b18e0bf9fbf16503413f5a5c9716b15d3
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-10 09:36:59 +0200

    Create store.jsx

---
+++ src/redax/store.jsx
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+const persistConfig = {
+  key: 'stateContacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
